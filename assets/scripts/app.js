46c41580621ac75f48e97536179e83859ba4b489
FrancaTM <francatm@outlook.com>
2019-11-07 14:41:38 -0300

    refactor code and add constant identifiers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 10;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK = "ATTACK"; //MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = "STRONG_ATTACK"; //MODE_STRONG_ATTACK = 1
+
 const enteredValue = prompt("Maximum life for you and the monster:", "100");
 
 let chosenMaxLife = parseInt(enteredValue);
@@ -55,9 +58,9 @@ function endRound() {
 function attackMonster(mode) {
   let maxDamage;
 
-  if (mode === "ATTACK") {
+  if (mode === MODE_ATTACK) {
     maxDamage = ATTACK_VALUE;
-  } else if (mode === "STRONG_ATTACK") {
+  } else if (mode === MODE_STRONG_ATTACK) {
     maxDamage = STRONG_ATTACK_VALUE;
   }
 
@@ -68,11 +71,11 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-  attackMonster("ATTACK");
+  attackMonster(ATTACK_VALUE);
 }
 
 function strongAttackHandler() {
-  attackMonster("STRONG_ATTACK");
+  attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
