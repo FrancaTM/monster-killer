82f69197bba6a114f018c6745062a38132ef74ba
FrancaTM <francatm@outlook.com>
2019-11-25 14:39:32 -0300

    refactor code to show log entries

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -188,7 +188,20 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  console.log(battleLog);
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log(battleLog[i]);
+  // }
+
+  let i = 0;
+  for (const logEntry of battleLog) {
+    console.log(`#${i}`);
+
+    for (const key in logEntry) {
+      console.log(`${key} => ${logEntry[key]}`);
+    }
+
+    i++;
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
